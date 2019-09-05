fac27b6aaef8eb387d883ebb5af8e86996f6a1e4
BW05GuoJianChao <2605579851@qq.com>
2019-09-05 14:29:51 +0800

    第一次版本

---
+++ src/main/java/com/bw/entity/demo.java
@@ -0,0 +1,15 @@
+package com.bw.entity;
+/**
+ * @作者 国建超
+ * @日期 2019年9月5日
+ * @时间 下午2:28:39
+ * 类功能：
+ *
+ */
+public class demo {
+   public static void main(String[] args) {
+	
+	   System.out.println("hellow");
+	   
+}
+}
